24e6c12f45c0026ef09bdabaa1a963c9909bcb3c
mayukhaar <73047883+mayukhaar@users.noreply.github.com>
2020-11-28 00:26:00 -0500

    Update Text-Adventure.py

--- Text-Adventure.py
+++ Text-Adventure.py
@@ -6,9 +6,23 @@ still = "c) Stay still and wait for whoever it is to catch up to you. \n"
 print("You have three options. \n" + str(walk) + str(run) + str(still))
 answer_1 = input("What do you do?: ")
 if answer_1 == "a":
-    print("ello mate")
+    print("The footsteps quicken with yours. Your walk turns to jogging, ")
+    print("which quickly becomes running as fast as you possibly can. You")
+    print("see a dark alley nearby and duck in to escape. ")
+
+    #nearby alley
+    #duck in to escape
+    #fall down a hole
+    #see a circus performing in front of you
+
+
 elif answer_1 == "b":
     print("do something")
+    #you are almost home when it begins to rain
+    #its slippery and you fall
+    #the person catches up to you
+    #
+    #   eyes glint mysteriously
 elif answer_1 == "c":
     print("hello scary person chasing me")
 else:
