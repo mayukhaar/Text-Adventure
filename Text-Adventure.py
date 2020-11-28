2889f96fce25525ccfb11d5a9d14d0d3fe5e577c
mayukhaar <73047883+mayukhaar@users.noreply.github.com>
2020-11-27 23:37:48 -0500

    First Code
    This code is the basis, shows intro + question 1.

---
+++ Text-Adventure.py
@@ -0,0 +1,7 @@
+print("It’s dark earlier than you expected. You’re walking home, and \nare nearly there until, suddenly, you hear footsteps behind you.")
+print("You glance back, but you see no one there. The footsteps draw closer. \nWhat do you do?")
+walk = "a) Walk away quickly. \n"
+run = "b) Start running home. \n"
+still = "c) Stay still and wait for whoever it is to catch up to you. \n"
+print("You have three options. \n" + str(walk) + str(run) + str(still))
+answer_1 = input("What do you do?: ")
