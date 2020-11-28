5b96cf4f674086089d312f6771bd66f4c2451eef
mayukhaar <73047883+mayukhaar@users.noreply.github.com>
2020-11-27 23:54:26 -0500

    Update Text-Adventure.py

--- Text-Adventure.py
+++ Text-Adventure.py
@@ -5,3 +5,11 @@ run = "b) Start running home. \n"
 still = "c) Stay still and wait for whoever it is to catch up to you. \n"
 print("You have three options. \n" + str(walk) + str(run) + str(still))
 answer_1 = input("What do you do?: ")
+if answer_1 == "a":
+    print("ello mate")
+elif answer_1 == "b":
+    print("do something")
+elif answer_1 == "c":
+    print("hello scary person chasing me")
+else:
+    print("I don't recognize that response.")
