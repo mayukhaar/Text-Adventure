1053debb5aea5b6af0756c6ac74f2177a9703926
mayukhaar <73047883+mayukhaar@users.noreply.github.com>
2020-12-05 00:21:49 -0500

    Update Text-Adventure.py

--- Text-Adventure.py
+++ Text-Adventure.py
@@ -5,17 +5,18 @@ print("front of you. Still, the streetlights are shining faintly near your")
 print("house, and you're almost home\nuntil")
 print("you hear footsteps behind you. You glance back and there's no")
 print("one there that you can see.")
-print("\nYou have three options that you are considering:")
+options = ("\nYou have three options that you are considering:")
 walk = "a) Walk away quickly. \n"
 run = "b) Start running home. \n"
 still = "c) Stay still and wait for whoever it is to catch up to you. \n"
-print("You have three options. \n" + str(walk) + str(run) + str(still))
+print(str(options))
+print(str(walk) + str(run) + str(still))
 answer_1 = input("What do you do?: ")
 if answer_1 == "a":
     print("The footsteps quicken with yours. Your walk turns to jogging, ")
     print("which quickly becomes running as fast as you possibly can. You")
     print("see a dark alley nearby and duck in to escape. ")
-
+    print("(Character Inventory: 2 people)")
 
 
    # print("Sorry, I don't recognize that response.")
@@ -33,20 +34,21 @@ elif answer_1 == "b":
     print("slippery, and you fall. The person chasing you catches up to")
     print("you. You are expecting it to be someone ominous, but you are")
     print("relieved to see that it is one of your friends. Strangely though")
-    print("they're eyes seem to glow bright red in the darkness.")
+    print("their eyes seem to glow bright red in the darkness.")
     #add another option somewhere around her talking about the
     # slippery road and what you do then
     ignore = "a) Ignore it and start walking home again, this time with them. \n"
     glow = "b) Ask them why their eyes glowed. \n"
     ask = "c) Ignore their eye color and ask why they were following you. \n"
-    print("You have three options: " + str(ignore) + str(glow) + str(ask))
+    print(options)
+    print(str(ignore) + str(glow) + str(ask))
     answer_2 = input("What do you do?: ")
     if answer_2 == "a":
         print("You reach home safely, but your friend's eyes glint strangely")
         print("again the second you reach the door. You regret not asking")
         print("them why they were following you. ")
     elif answer_2 == "b":
-        print("")
+        print("'Oh, that?' they laugh. ")
     elif answer_2 == "c":
         print("something ")
     else:
